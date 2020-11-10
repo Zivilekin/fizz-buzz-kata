a487a45320e81a6994336c8df8fa6c7e52a09358
zivilekin <zivilek@wix.com>
2020-11-10 11:14:39 +0200

    fizz buzz

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function fizzBuzz() {
-  return [];
+function fizzBuzz(min = 1, max = 100, rules = []) {
+  let output = [];
+  for (let i = min; i <= max; i++) {
+    let str = '';
+
+    rules.forEach((rule) => {
+      if (rule.condition(i)) {
+        str += rule.output;
+      }
+    });
+
+    output.push(str ? str : i);
+  }
+
+  return output;
 }
 
 module.exports.fizzBuzz = fizzBuzz;
--- index.test.js
+++ index.test.js
@@ -1,62 +1,98 @@
 const { fizzBuzz } = require('./index');
 const fixtures = require('./fixtures');
 
+const baseRules = [
+  { condition: (i) => i % 3 === 0, output: 'Fizz' },
+  { condition: (i) => i % 5 === 0, output: 'Buzz' },
+];
+
 describe('fizzBuzz', () => {
   test('should return original', () => {
-    expect(fizzBuzz()).toEqual(fixtures.fizzBuzz);
+    expect(fizzBuzz(1, 100, baseRules)).toEqual(fixtures.fizzBuzz);
   });
 
   describe('step 1', () => {
-    test.skip('should work with range 1 to 20', () => {
-      expect(fizzBuzz()).toEqual(fixtures.fizzBuzzFrom1to20);
+    test('should work with range 1 to 20', () => {
+      expect(fizzBuzz(1, 20, baseRules)).toEqual(fixtures.fizzBuzzFrom1to20);
     });
 
-    test.skip('should work with range 15 to 50', () => {
-      expect(fizzBuzz()).toEqual(fixtures.fizzBuzzFrom15to50);
-    })
+    test('should work with range 15 to 50', () => {
+      expect(fizzBuzz(15, 50, baseRules)).toEqual(fixtures.fizzBuzzFrom15to50);
+    });
   });
 
   describe('step 2', () => {
-    test.skip('should work with additional Foo rule', () => {
-      expect(fizzBuzz()).toEqual(fixtures.fizzBuzzFoo);
+    const fooRule = { condition: (i) => i % 7 === 0, output: 'Foo' };
+    const booRule = { condition: (i) => i % 11 === 0, output: 'Boo' };
+
+    test('should work with additional Foo rule', () => {
+      expect(fizzBuzz(1, 100, [...baseRules, fooRule])).toEqual(
+        fixtures.fizzBuzzFoo
+      );
     });
 
-    test.skip('should work with additional Boo rule', () => {
-      expect(fizzBuzz()).toEqual(fixtures.fizzBuzzBoo);
+    test('should work with additional Boo rule', () => {
+      expect(fizzBuzz(1, 100, [...baseRules, booRule])).toEqual(
+        fixtures.fizzBuzzBoo
+      );
     });
 
-    test.skip('should work with both additional Foo and Boo rules', () => {
-      expect(fizzBuzz()).toEqual(fixtures.fizzBuzzFooBoo);
+    test('should work with both additional Foo and Boo rules', () => {
+      expect(fizzBuzz(1, 100, [...baseRules, fooRule, booRule])).toEqual(
+        fixtures.fizzBuzzFooBoo
+      );
     });
   });
 
   describe('step 3', () => {
-    test.skip('should work with Small rule', () => {
-      expect(fizzBuzz()).toEqual(fixtures.smallFizzBuzz);
+    smallRule = { condition: (i) => i < 16, output: 'Small' };
+    bigRule = { condition: (i) => i > 95, output: 'Big' };
+
+    test('should work with Small rule', () => {
+      expect(fizzBuzz(1, 100, [smallRule, ...baseRules])).toEqual(
+        fixtures.smallFizzBuzz
+      );
     });
 
-    test.skip('should work with Big rule', () => {
-      expect(fizzBuzz()).toEqual(fixtures.bigFizzBuzz);
+    test('should work with Big rule', () => {
+      expect(fizzBuzz(1, 100, [bigRule, ...baseRules])).toEqual(
+        fixtures.bigFizzBuzz
+      );
     });
   });
 
   describe('step 4', () => {
-    test.skip('should work as BuzzFizz', () => {
-      expect(fizzBuzz()).toEqual(fixtures.buzzFizz);
+    const fizzRule = { condition: (i) => i % 3 === 0, output: 'Fizz' };
+    const buzzRule = { condition: (i) => i % 5 === 0, output: 'Buzz' };
+
+    test('should work as BuzzFizz', () => {
+      expect(fizzBuzz(1, 100, [buzzRule, fizzRule])).toEqual(fixtures.buzzFizz);
     });
 
-    test.skip('should work as BuzzSmallFizz', () => {
-      expect(fizzBuzz()).toEqual(fixtures.buzzSmallFizz);
+    test('should work as BuzzSmallFizz', () => {
+      expect(fizzBuzz(1, 100, [buzzRule, smallRule, fizzRule])).toEqual(
+        fixtures.buzzSmallFizz
+      );
     });
   });
 
   describe('step 5', () => {
-    test.skip('should work with rule FTW only', () => {
-      expect(fizzBuzz()).toEqual(fixtures.ftw);
+    const ftwRule = {
+      condition: (i) => i % 3 === 0 && i % 5 === 0,
+      output: 'FTW',
+    };
+
+    const ggRule = {
+      condition: (i) => i % 3 === 0 || i % 5 === 0,
+      output: 'GG',
+    };
+
+    test('should work with rule FTW only', () => {
+      expect(fizzBuzz(1, 100, [ftwRule])).toEqual(fixtures.ftw);
     });
 
-    test.skip('should work with rule GG only', () => {
-      expect(fizzBuzz()).toEqual(fixtures.gg);
+    test('should work with rule GG only', () => {
+      expect(fizzBuzz(1, 100, [ggRule])).toEqual(fixtures.gg);
     });
   });
 });
